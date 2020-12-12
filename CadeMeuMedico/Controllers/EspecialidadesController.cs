3eb4b6a4ecbf119b9ac6676cbd611dba22d81bc4
Marcos Keller <marcoskeller@hotmail.com>
2020-12-12 10:46:01 -0300

    Criacao da Controller Especialidades e do Metodo Adicionar e de suas Views

---
+++ CadeMeuMedico/Controllers/EspecialidadesController.cs
@@ -0,0 +1,42 @@
+﻿using CadeMeuMedico.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadeMeuMedico.Controllers
+{
+    public class EspecialidadesController : Controller
+    {
+        private EntidadesCadeMeuMedicoBD db = new EntidadesCadeMeuMedicoBD();
+
+        
+        public ActionResult Index()
+        {
+            var especialidades = db.Especialidades.ToList();
+            
+            return View(especialidades);
+        }
+
+        public ActionResult Adicionar()
+        {
+            ViewBag.IDEspecialidade = new SelectList(db.Especialidades, "IDEspecialidade", "Nome");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Especialidades especialidade)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Especialidades.Add(especialidade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(especialidade);
+        }
+    }
+}
